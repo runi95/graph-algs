1d5cd3715eae4b7c2da90c27031b7be44b8d0029
runi95 <runar.serigstad@gmail.com>
2022-01-12 18:32:12 +0100

    Removed deprecated @mui code

--- portal/src/index.js
+++ portal/src/index.js
@@ -3,28 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createTheme, ThemeProvider} from '@mui/material';
-
-const darkTheme = createTheme({
-  components: {
-    MuiRadio: {
-      styleOverrides: {
-        root: {
-          padding: 6,
-        },
-      },
-    },
-  },
-  palette: {
-    mode: 'dark',
-  },
-});
 
 ReactDOM.render(
     <React.StrictMode>
-      <ThemeProvider theme={darkTheme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </React.StrictMode>,
     document.getElementById('root'),
 );
