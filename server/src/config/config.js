f21f254b48346b1e39ab52c863bd98ba9c26d213
runi95 <runar.serigstad@gmail.com>
2022-02-12 00:55:31 +0100

    Fixed issues with config loading from different working directories

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,3 +1,3 @@
 exports.port = process.env.PORT || 8080;
 exports.logLevel = process.env.LOG_LEVEL || 'debug';
-exports.templatesDir = process.env.TEMLATES_DIR || 'src/templates';
+exports.templatesDir = process.env.TEMLATES_DIR || `${__dirname}/../templates`;
