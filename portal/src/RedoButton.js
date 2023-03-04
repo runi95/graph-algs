924bd3111cf19ce02517fad2bad74cf4bc1dfeed
runi95 <runar.serigstad@gmail.com>
2023-03-04 18:42:20 +0100

    Added svg icon outlines

--- portal/src/RedoButton.js
+++ portal/src/RedoButton.js
@@ -3,7 +3,7 @@ import React from 'react';
 function UndoButton(_props) {
   return (
     <svg color="#555" transform="scale(1, -1)" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="100%" height="100%" viewBox="0 0 512 512" xmlSpace="preserve">
-      <path fill="currentcolor" d="M0,32c0,64,96,192,320,192V96l192,192L320,480V352C143.563,352,0,208.437,0,32z"/>
+      <path stroke='#888' strokeWidth='24px' fill="currentcolor" d="M0,32c0,64,96,192,320,192V96l192,192L320,480V352C143.563,352,0,208.437,0,32z"/>
     </svg>
   );
 }
